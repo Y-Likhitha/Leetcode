d4d9d79dc5fa6ff0a49f931a1ece830e969690fc
Y-Likhitha <136236883+Y-Likhitha@users.noreply.github.com>
2026-06-06 19:30:36 +0530

    Time: 3 ms (26.32%), Space: 19.3 MB (19.13%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            ls=sum(nums[0:i])
+            rs=sum(nums[i+1:len(nums)])
+            if ls==rs:
+               return i 
+        return -1
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
